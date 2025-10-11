14587f5332fd03331813473078590ce7443d06e9
Susana Madriz <139632743+susanamadriz@users.noreply.github.com>
2025-10-11 23:39:23 +0200

    arreglando normi

--- src/hook.c
+++ src/hook.c
@@ -3,31 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   hook.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sjuan-ma <sjuan-ma@student.42.fr>          +#+  +:+       +#+        */
+/*   By: susanamadriz <susanamadriz@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 19:51:11 by sjuan-ma          #+#    #+#             */
-/*   Updated: 2025/09/18 19:19:16 by sjuan-ma         ###   ########.fr       */
+/*   Updated: 2025/10/11 23:38:56 by susanamadri      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void my_keyhook(mlx_key_data_t keydata, void *param)
+void	my_keyhook(mlx_key_data_t keydata, void *param)
 {
-    t_game *game = param;
+	t_game	*game;
 
-    if (keydata.action == MLX_PRESS) // solo al presionar
-    {
-        if (keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP)
-            move_player(game, 0, -1);
-        if (keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN)
-            move_player(game, 0, 1);
-        if (keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_LEFT)
-            move_player(game, -1, 0);
-        if (keydata.key == MLX_KEY_D || keydata.key == MLX_KEY_RIGHT)
-            move_player(game, 1, 0);
-        if (keydata.key == MLX_KEY_ESCAPE)
-            mlx_close_window(game->mlx);
-    }
+	game = param;
+
+	if (keydata.action == MLX_PRESS)
+	{
+		if (keydata.key == MLX_KEY_W || keydata.key == MLX_KEY_UP)
+			move_player(game, 0, -1);
+		if (keydata.key == MLX_KEY_S || keydata.key == MLX_KEY_DOWN)
+			move_player(game, 0, 1);
+		if (keydata.key == MLX_KEY_A || keydata.key == MLX_KEY_LEFT)
+			move_player(game, -1, 0);
+		if (keydata.key == MLX_KEY_D || keydata.key == MLX_KEY_RIGHT)
+			move_player(game, 1, 0);
+		if (keydata.key == MLX_KEY_ESCAPE)
+			mlx_close_window(game->mlx);
+	}
 }
 
